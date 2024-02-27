15d03573a453eb7b51224b58f8072f1f82b4f21f
Étienne Boucher <etienneboucher@hotmail.ca>
2024-02-27 15:33:36 -0500

    Duplicate original BaseEncoder and BaseDecoder temporarely
    Add a copy of the original BaseEncoder and BaseDecoder to use in model not already kerasized.
    Expose them in submodule.

--- src/pythae/models/nn/__init__.py
+++ src/pythae/models/nn/__init__.py
@@ -3,6 +3,6 @@ In this module are stored the main Neural Networks Architectures.
 """
 
 
-from .base_architectures import BaseDecoder, BaseDiscriminator, BaseEncoder, BaseMetric
+from .base_architectures import BaseDecoder, BaseDiscriminator, BaseEncoder, BaseMetric, BaseDecoder_PT, BaseEncoder_PT
 
-__all__ = ["BaseDecoder", "BaseEncoder", "BaseMetric", "BaseDiscriminator"]
+__all__ = ["BaseDecoder", "BaseEncoder", "BaseMetric", "BaseDiscriminator", "BaseDecoder_PT", "BaseEncoder_PT"]
